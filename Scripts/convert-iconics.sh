96873f4c987c5decc544e5853d89947f9af14c0d
Marcus Downing <marcus@bang-on.net>
2012-11-11 02:08:51 +0000

    Convert iconics - sub folders

--- Scripts/convert-iconics.sh
+++ Scripts/convert-iconics.sh
@@ -13,7 +13,7 @@ do
     ebegin "$FB/${GOOD}$FN"
     mkdir -p "../Composer/public/images/iconics/$FB/Large"
     mkdir -p "../Composer/public/images/iconics/$FB/Small"
-    convert -bordercolor white -border 10x10 "$F" png:- | ../Scripts/magicwand 0,0 -t 2 -r outside -c trans png:- png:- | convert -trim png:- "$TARGET/$FB/Large/$FN.png"
+    convert -bordercolor white -border 10x10 "$F" png:- | ../Scripts/magicwand 0,0 -t 4 -r outside -c trans png:- png:- | convert -trim png:- "$TARGET/$FB/Large/$FN.png"
     eend $?
     convert -resize 500x500 "$TARGET/$FB/Large/$FN.png" "$TARGET/$FB/Small/$FN.png"
   fi
