806493209a761b16c4f6fbb03d70a9c1456d32f7
Marcus Downing <marcus@bang-on.net>
2013-02-12 00:25:39 +0000

    Collect all 3.5

--- Scripts/i18n collect all.jsx
+++ Scripts/i18n collect all.jsx
@@ -23,6 +23,32 @@ i18n.extractFolder(new Folder(base+'Pathfinder/GM'));
 
 i18n.extractFolder(new Folder(base+'Pathfinder/Psionics'));
 i18n.extractFolder(new Folder(base+'Pathfinder/Tome of Secrets'));
+i18n.extractFolder(new Folder(base+'Pathfinder/NeoExodus'));
+
+i18n.extractFolder(new Folder(base+'All'));
+i18n.extractFolder(new Folder(base+'Extra'));
 i18n.saveCSV(new File(base+'Languages/Template/Pathfinder - Everything.csv'));
+
+
+// 3.5 core
+i18n.init();
+
+i18n.extractFile(new File(base+'3.5/Core/Character Info - Simple.ai'));
+i18n.extractFile(new File(base+'3.5/Core/Character Info.ai'));
+i18n.extractFile(new File(base+'3.5/Core/Blank Character Info.ai'));
+i18n.extractFile(new File(base+'3.5/Core/Combat - Simple.ai'));
+i18n.extractFile(new File(base+'3.5/Core/Combat.ai'));
+i18n.extractFolder(new Folder(base+'3.5/Core'));
+i18n.extractFolder(new Folder(base+'3.5/Barbarian'));
+i18n.saveCSV(new File(base+'Languages/Template/3.5 - Core.csv'));
+
+i18n.extractFolder(new Folder(base+'3.5/Extended'));
+i18n.extractFolder(new Folder(base+'3.5/Variants'));
+i18n.extractFolder(new Folder(base+'3.5/Dragon Compendium'));
+i18n.extractFolder(new Folder(base+'3.5/Incarnum'));
+i18n.extractFolder(new Folder(base+'3.5/Psionics'));
+i18n.extractFolder(new Folder(base+'3.5/Tomes'));
+i18n.saveCSV(new File(base+'Languages/Template/3.5 - Everything.csv'));
+
 
 alert("Done");
