410b4c8470c861384cd22c3dca161ad8afc6e966
Marcus Downing <marcus@bang-on.net>
2013-07-17 22:29:16 +0100

    Play version update

--- Composer/project/Build.scala
+++ Composer/project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,25 +8,26 @@ object ApplicationBuild extends Build {
     val appVersion      = "2.0"
 
     //val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
-    val typesafe = "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
-    val typesafeSnapshots = "Typesafe Snaps Repo" at "http://repo.typesafe.com/typesafe/snapshots/"
     //val jerksonRels = "repo.codahale.com" at "http://repo.codahale.com"
+
+    // val typesafe = "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+    // val typesafeSnapshots = "Typesafe Snaps Repo" at "http://repo.typesafe.com/typesafe/snapshots/"
     
     val appDependencies = Seq(
       // Add your project dependencies here
       //"org.mindrot" % "jbcrypt" % "0.3m",
-      "joda-time" % "joda-time" % "2.1",
+      "joda-time" % "joda-time" % "2.2",
       //"com.mongodb.casbah" %% "casbah" % "2.1.5-1",
-      "org.mongodb" %% "casbah" % "2.4.1",
+      "org.mongodb" %% "casbah" % "2.6.2",
       "com.novus" %% "salat-core" % "1.9.1",
       //"com.lowagie" % "itext" % "2.1.7"
-      "com.itextpdf" % "itextpdf" % "5.3.2",
-      "org.apache.commons" % "commons-email" % "1.2"
+      "com.itextpdf" % "itextpdf" % "5.4.2",
+      "org.apache.commons" % "commons-email" % "1.3.1"
       //"net.debasishg" %% "sjson" % "0.17"
       //"com.codahale" %% "jerkson" % "0.5.0"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
