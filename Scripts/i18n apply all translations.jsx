bb32564b2c76dac64981e52672c0a44f6f911bd6
Marcus Downing <marcus@bang-on.net>
2015-03-02 00:48:31 +0000

    i18n scripts fixes

--- Scripts/i18n apply all translations.jsx
+++ Scripts/i18n apply all translations.jsx
@@ -7,20 +7,22 @@
 
 i18n.init();
 
-var folders = [ "Pathfinder", "3.5", "All" ];
-
-var languages = [ "Italian", "Spanish", "Polish", "French", "Portuguese", "Russian" ];
-
+var folders = [ "Pathfinder", "3.5", "All", "Extra" ];
+var languages = [ "Italian", "Spanish", "Polish", "German", "French", "Portuguese", "Russian", "US English" ];
+log("i18n: Preparing to translate into", languages);
 
 for (var i = 0; i < languages.length; i++) {
 	var language = languages[i];
 
-	i18n.loadTranslations(baseFolder + '/Languages/' + language + '.csv');
+	i18n.loadTranslations(new File(baseFolder + 'Languages/' + language + '.csv'));
 
-	for (var j = 0; j < folders; j++) {
+	for (var j = 0; j < folders.length; j++) {
 		var folder = folders[j];
-		var srcFolder = baseFolder + '/' + folder;
-		var dstFolder = baseFolder + '/Languages/' + language + '/' + folder;
-		i18.applyTranslationsFolder(srcFolder, dstFolder);
+		var srcFolder = new Folder(baseFolder + folder);
+		var dstFolder = new Folder(baseFolder + 'Languages/' + language + '/' + folder);
+		i18n.applyTranslationsFolder(srcFolder, dstFolder);
 	}
 }
+
+log("i18n: Translated "+i18n.countTranslatedLines+" strings from "+i18n.countTranslatedFiles+" files");
+alert("Done!");
