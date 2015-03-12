ee022f012f23d0fedf3165b35b2d27c7812520e6
Marcus Downing <marcus@bang-on.net>
2015-03-12 19:02:30 +0000

    Iconics script: make it more portable

--- Scripts/convert-iconics.sh
+++ Scripts/convert-iconics.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-source /etc/init.d/functions.sh
-eval $(eval_ecolors)
 
 SOURCE="../Iconics"
 TARGET="../../Character Sheets Website/public/images/iconics";
 TXT="$TARGET/iconics.txt"
 echo -n "" > "$TXT"
-cd ../Iconics
+cd "$SOURCE"
 find . -type f | while read F
 do
   F=$(echo "$F" | sed 's/^..//')
@@ -15,24 +13,28 @@ do
   if [ "$FB" != "." ]
   then
     NAME="$FB/$FN"
-    LOC=$(echo "$FB/$FN" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9\/]\+/-/g')
-    LARGE="$TARGET/large/$LOC.png"
-    SMALL="$TARGET/small/$LOC.png"
+    LOC=$(echo "$FB/$FN" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9\/]\+/-/g' | sed 's/^-\+//' | sed 's/-\+$//')
 
-    einfo "$NAME"
-    eindent
+    if "$F" | grep '.txt$'; then
+      echo " * $NAME"
+      DF="$TARGET/large/$LOC.txt"
+      echo 
+      cp "$F" "$DF"
+    else
+      LARGE="$TARGET/large/$LOC.png"
+      SMALL="$TARGET/small/$LOC.png"
 
-    ebegin "Large file: $LARGE"
-    mkdir -p "$(dirname "$LARGE")"
-    convert -bordercolor white -border 10x10 "$F" png:- | ../Scripts/magicwand 0,0 -t 4 -r outside -c trans png:- png:- | convert -trim png:- "$LARGE"
-    eend $?
+      echo " * $NAME"
 
-    ebegin "Small file: $SMALL"
-    mkdir -p "$(dirname "$SMALL")"
-    convert -resize 300x400 "$LARGE" "$SMALL"
-    eend $?
+      echo "   * Large file: $LARGE"
+      mkdir -p "$(dirname "$LARGE")"
+      convert -bordercolor white -border 10x10 "$F" png:- | ../Scripts/magicwand 0,0 -t 4 -r outside -c trans png:- png:- | convert -trim png:- "$LARGE"
 
-    echo "$LOC=$NAME" >> "$TXT"
-    eoutdent
+      echo "   * Small file: $SMALL"
+      mkdir -p "$(dirname "$SMALL")"
+      convert -resize 300x400 "$LARGE" "$SMALL"
+
+      echo "$LOC=$NAME" >> "$TXT"
+    fi
   fi
 done
